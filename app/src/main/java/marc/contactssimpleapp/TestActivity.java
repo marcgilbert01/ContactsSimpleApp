f02a5d400ab9aaa6d53c38f322a00c7ebe07fab6
marcgilbert01 <marcgilbert01@gmail.com>
2016-04-21 16:05:01 +0100

    - test second commit

--- app/src/main/java/marc/contactssimpleapp/TestActivity.java
+++ app/src/main/java/marc/contactssimpleapp/TestActivity.java
@@ -7,6 +7,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+//  TEST NEW COMMIT
+
+
 public class TestActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
